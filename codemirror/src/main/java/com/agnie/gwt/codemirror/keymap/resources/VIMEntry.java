9fbbaba4b93dcbbf85c2e891aa361457b85a7be3
pandurangpatil <pandurang@agnie.net>
2014-12-04 20:27:42 +0530

    Made changes to load Js files through respective entry point classes to support xsiframe mode.

---
+++ codemirror/src/main/java/com/agnie/gwt/codemirror/keymap/resources/VIMEntry.java
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ * MIT License (MIT) © 2014 Copyright Agnie Media Software Pvt. Ltd.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ ******************************************************************************/
+package com.agnie.gwt.codemirror.keymap.resources;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.ScriptInjector;
+
+/**
+ * @author Pandurang Patil 04-Dec-2014
+ *
+ */
+public class VIMEntry implements EntryPoint {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.google.gwt.core.client.EntryPoint#onModuleLoad()
+	 */
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		ScriptInjector.fromString(KeyMapResources.INSTANCE.vim().getText()).setWindow(ScriptInjector.TOP_WINDOW).inject();
+	}
+
+}
